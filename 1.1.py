2cf4da26f96eef803b2ceff1694ff8c23c270142
vignesh2431 <147687340+vignesh2431@users.noreply.github.com>
2023-10-17 14:09:10 +0530

    Add files via upload

---
+++ 1.1.py
@@ -0,0 +1,10 @@
+def factorial(n):
+  if n == 0:
+      return 1
+  else:
+      return n * factorial(n - 1)
+
+# Test the function with a sample input
+number = 5
+result = factorial(number)
+print(f"The factorial of {number} is {result}")
